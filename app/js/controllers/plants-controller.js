535643e54571b6610db08da632d4b73542f7a14a
Jørgen Hookham <JorgenHookham@Gmail.com>
2013-04-09 22:22:31 -0700

    Refactor appearance.

--- app/js/controllers/plants-controller.js
+++ app/js/controllers/plants-controller.js
@@ -3,7 +3,7 @@
 
 watersched.controller('PlantsCtrl',
     function PlantsCtrl ($scope, storage_service) {
-        
+
         $scope.plants = [];
 
         storage_service.fetchAll($scope);
@@ -23,6 +23,10 @@ watersched.controller('PlantsCtrl',
         $scope.removePlant = function (plant) {
             $scope.plants.pop(plant);
             storage_service.remove(plant);
+        };
+
+        $scope.next_water_day = function (plant) {
+            return new Date(plant.last_water.getTime() + plant.water*24*60*60*1000);
         };
 
 });
